b3e2bdfe1aa43a603de28047061d1c5f3a049e6e
LongLH <long.luc@ntq-solution.com.vn>
2017-10-20 14:10:00 +0700

    support CONFIG arg

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const defautsDeep = require('lodash.defaultsdeep');
 const deepFreeze = require('deep-freeze');
+const path = require('path');
 const omit = require('object.omit');
 const yargs = require('yargs');
 
 // remove yargs properties and ENV
-const args = omit(yargs.argv, ['_', 'help', 'version', '$0', 'ENV']);
+const args = omit(yargs.argv, ['_', 'help', 'version', '$0', 'ENV', 'CONFIG']);
 
 function load({ defaults = {}, file, respectENV = false, defaultENV }) {
-  let output = file ? require(file) : {};
+  let configPath = yargs.argv.CONFIG || file;
+  let output = configPath ? require(path.resolve('.', configPath)) : {};
 
   if (respectENV) {
     let env = process.env.NODE_ENV || yargs.argv.ENV || defaultENV;
